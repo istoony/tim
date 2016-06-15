cce095a189dfc45fd3a2a273bc0b3fbb9c0a6fee
Matteo Foglio <matteo.foglio@mail.polimi.it>
2016-06-15 16:03:03 +0200

    alpha template

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
 
+	//SMALL MENU
+	var isHidden = true;
+	
+	$("#top-menu-small .first-menu").hide();
+	$("#top-menu-small .second-menu").hide();
+	$("#top-menu-button-small .menu-open").hide();
+	
+	
+	 $("#top-menu-button-small").click(function(){
+		 
+		 if(isHidden == true ) {
+			 isHidden = false;
+			 $("#top-menu-button-small .menu-closed").hide();
+			 $("#top-menu-button-small .menu-open").show();
+			 $("#top-menu-small .first-menu").fadeIn(500);
+			 $("#top-menu-small .second-menu").fadeIn(800);
+		 }
+		 else {	
+			 isHidden = true;
+			 $("#top-menu-button-small .menu-open").hide();
+			 $("#top-menu-button-small .menu-closed").show();
+			 $("#top-menu-small .first-menu").fadeOut(500);
+			 $("#top-menu-small .second-menu").fadeOut(800);
+		 }
+	 });
+	
+	
+	
+	
+	//CONTROL LOGIC
+	
+	
+	
 	checkURL();	//check if the URL has a reference to a page and load it
 
 	$('#top ul li a, #footer ul li a').click(function (e){
